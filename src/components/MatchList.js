fa4e4567a32ce7c3f7ebfc094b8ab2800d675636
“mehmetkah” <mehmetkahca@gmail.com>
2022-11-01 00:14:35 -0600

    conditional rendering

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -1,10 +1,16 @@
 import Match from "./Match";
 import matchData from "./data/matchData";
+
 function MatchList() {
+  const oneMatch = matchData[0];
   return (
     <section className="PlayerList MatchList">
       <h1>Match list</h1>
-      <Match />
+      <Match
+        players={oneMatch.players}
+        winner={oneMatch.winner}
+        scoreDifference={oneMatch.scoreDifference}
+      />
     </section>
   );
 }
